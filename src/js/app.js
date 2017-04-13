8a7dc0dff8edd91162cb376000eca9cdede491f6
jc_drelkin <drelkin@justcoded.co>
2017-04-13 14:55:18 +0300

    create nesting functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,32 @@
 function RadialMenu() {
-  this.menu = document.createElement('ul');
-  this.menu.classList.add('radial-menu');
-  this.li = document.createElement('li');
-  this.li.classList.add('radial-menu-item');
-  document.body.appendChild(this.menu);
-  // this.count = 1;
-  //New Item
-  this.item = this.li.appendChild(this.menu);
+    this.menu = document.createElement('ul');
+    this.menu.classList.add('radial-menu');
+    document.body.appendChild(this.menu);
+    return this;
 }
 
-RadialMenu.prototype.add = function() {
-    // let that = this;
-    // this.appendChild(this.item);
-  // this.li.innerHTML = `${text}`;
-  // this.menu.appendChild(this.li.cloneNode(true));
-  console.log(new RadialMenu());
+RadialMenu.prototype.add = function(text='item') {
+  return new RadialMenuItem(this, text);
 };
-// var newLi = document.createElement('li');
-//   newLi.innerHTML = 'Привет, мир!';
-// var textElem = document.createTextNode('');
-// var div = document.createElement('div');
-// div.innerHTML = "<strong>Ура!</strong>";
 
-let menu = new RadialMenu();
-console.log(menu);
+function RadialMenuItem(parent, text) {
+  this.li = document.createElement('li');
+  this.li.innerHTML = `${text}`;
+  this.li.classList.add('inner-menu-item');
+  this.menu = document.createElement('ul');
+  this.menu.classList.add('inner-menu');
+  this.createStructure(parent);
+}
+
+RadialMenuItem.prototype.createStructure = function (parent) {
+  parent.menu.appendChild(this.li);
+  this.li.appendChild(this.menu);
+}
+
+RadialMenuItem.prototype.add = function (text='item-inner') {
+  return new RadialMenuItem(this, text);
+}
+
+let generalMenu = new RadialMenu();
+let newItem1 = generalMenu.add('внешний');
+let newItem11 = newItem1.add('внутренний');
