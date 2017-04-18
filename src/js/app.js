90c61148d7c9979c249478d145adb2ec0fa0eeef
jc_drelkin <drelkin@justcoded.co>
2017-04-18 09:30:57 +0300

    small changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,5 @@
 function RadialMenu(styles={}) {
-
     let {borderColor, borderWith} = styles;
-
     this.menu = document.createElement('ul');
     this.menu.classList.add('radial-menu');
     this.menu.style.border = `${borderWith}`+'px solid '+`${borderColor}`;
@@ -9,10 +7,12 @@ function RadialMenu(styles={}) {
     return this;
 }
 
+//function aded item main item
 RadialMenu.prototype.add = function(text='item', styles={}) {
   return new RadialMenuItem(this, text, styles);
 };
 
+//finction for create main structure
 function RadialMenuItem(parent, text, styles={}) {
   this.style = styles;
   this.li = document.createElement('li');
@@ -27,16 +27,19 @@ function RadialMenuItem(parent, text, styles={}) {
   this.createStructure(parent);
 }
 
+//finction for create structure
 RadialMenuItem.prototype.createStructure = function (parent) {
   this.controlMenuItems();
   parent.menu.appendChild(this.li);
   this.li.appendChild(this.menu);
 }
 
+//function aded item child item
 RadialMenuItem.prototype.add = function (text='item-inner', styles={}) {
   return new RadialMenuItem(this, text, styles);
 }
 
+//function for control items menu
 RadialMenuItem.prototype.controlMenuItems = function () {
   this.li.onclick = function (e) {
     let openItems = document.querySelectorAll('.open-item');
@@ -88,14 +91,22 @@ let newItem1111 = newItem111.add('внутренний-3', {
   borderColor:'red',
   background:'blue'
 });
-// let newItem11111 = newItem1111.add('внутренний-4');
-// let newItem111111 = newItem11111.add('внутренний-5');
+let newItem11111 = newItem1111.add('внутренний-4');
+let newItem111111 = newItem11111.add('внутренний-5');
 
-let newItem2 = generalMenu.add('внешний-2');
+let newItem2 = generalMenu.add('внешний-2', {
+  borderWith:1,
+  borderColor:'red',
+  background:'#b8b8ff'
+});
 let newItem22 = newItem2.add('внутренний-2');
 let newItem222 = newItem22.add('внутренний-3');
 let newItem2222 = newItem222.add('внутренний-4');
 
-let newItem3 = generalMenu.add('внешний-3');
+let newItem3 = generalMenu.add('внешний-3', {
+  borderWith:1,
+  borderColor:'red',
+  background:'chartreuse'
+});
 let newItem33 = newItem3.add('внутренний-5');
 let newItem333 = newItem33.add('внутренний-6');
