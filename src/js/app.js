ab0716b02ecf32699362d3c857f6b0e798fd76bd
jc_drelkin <drelkin@justcoded.co>
2017-04-18 09:24:17 +0300

    style for child items

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,22 @@
-function RadialMenu(styles) {
+function RadialMenu(styles={}) {
+
+    let {borderColor, borderWith} = styles;
+
     this.menu = document.createElement('ul');
     this.menu.classList.add('radial-menu');
+    this.menu.style.border = `${borderWith}`+'px solid '+`${borderColor}`;
     document.body.appendChild(this.menu);
-    this.styleArg = {};
-    for (var style in styles) {
-      if (styles.hasOwnProperty(style)) {
-        this.styleArg[style] = styles[style];
-      }
-    }
     return this;
 }
 
-RadialMenu.prototype.add = function(text='item') {
-  return new RadialMenuItem(this, text, this.styleArg);
+RadialMenu.prototype.add = function(text='item', styles={}) {
+  return new RadialMenuItem(this, text, styles);
 };
 
-function RadialMenuItem(parent, text, style={}) {
-  this.style = style;
+function RadialMenuItem(parent, text, styles={}) {
+  this.style = styles;
   this.li = document.createElement('li');
-  let {borderColor, borderWith, background} = style;
+  let {borderColor, borderWith, background} = this.style;
   this.li.style.border = `${borderWith}`+'px solid '+`${borderColor}`;
   this.li.style.backgroundColor = `${background}`;
 
@@ -30,14 +28,13 @@ function RadialMenuItem(parent, text, style={}) {
 }
 
 RadialMenuItem.prototype.createStructure = function (parent) {
-
   this.controlMenuItems();
   parent.menu.appendChild(this.li);
   this.li.appendChild(this.menu);
 }
 
-RadialMenuItem.prototype.add = function (text='item-inner') {
-  return new RadialMenuItem(this, text, this.style);
+RadialMenuItem.prototype.add = function (text='item-inner', styles={}) {
+  return new RadialMenuItem(this, text, styles);
 }
 
 RadialMenuItem.prototype.controlMenuItems = function () {
@@ -68,16 +65,31 @@ RadialMenuItem.prototype.controlMenuItems = function () {
 
 let generalMenu = new RadialMenu({
   borderWith:1,
-  borderColor:'#f4c360',
-  background:'#ffeac1'
+  borderColor:'#f4c360'
 });
 
-let newItem1 = generalMenu.add('внешний-1');
-let newItem11 = newItem1.add('внутренний-1');
-let newItem111 = newItem11.add('внутренний-2');
-let newItem1111 = newItem111.add('внутренний-3');
-let newItem11111 = newItem1111.add('внутренний-4');
-let newItem111111 = newItem11111.add('внутренний-5');
+let newItem1 = generalMenu.add('внешний-1',{
+  borderWith:1,
+  borderColor:'red',
+  background:'yellow'
+});
+let newItem11 = newItem1.add('внутренний-1', {
+  borderWith:1,
+  borderColor:'green',
+  background:'red'
+});
+let newItem111 = newItem11.add('внутренний-2', {
+  borderWith:1,
+  borderColor:'orange',
+  background:'green'
+});
+let newItem1111 = newItem111.add('внутренний-3', {
+  borderWith:1,
+  borderColor:'red',
+  background:'blue'
+});
+// let newItem11111 = newItem1111.add('внутренний-4');
+// let newItem111111 = newItem11111.add('внутренний-5');
 
 let newItem2 = generalMenu.add('внешний-2');
 let newItem22 = newItem2.add('внутренний-2');
